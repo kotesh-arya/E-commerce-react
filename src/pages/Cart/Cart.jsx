eaefbb00ad555f3434b804824266185660a408bd
kotesh-arya <koteswarraomudila@gmail.com>
2022-08-22 20:45:55 +0530

    CHORES: removed commented code

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -27,7 +27,11 @@ function Cart() {
               <h2>
                 IS EMPTY😢 <br /> GO AND ADD ITEMS
               </h2>
-              <img className={CartCSS["empty-cart-image"]} src={EmptyCartImage} alt="" />
+              <img
+                className={CartCSS["empty-cart-image"]}
+                src={EmptyCartImage}
+                alt=""
+              />
             </div>
           ) : (
             <div className={`card ${CartCSS["cart-price-detail-card"]}`}>
@@ -80,49 +84,6 @@ function Cart() {
               </button>
             </div>
           )}
-          {/* <div className={`card ${CartCSS["cart-price-detail-card"]}`}>
-            <div className={CartCSS["price-card-title"]}>
-              <h1 className="header-small">PRICE DETAILS</h1>
-            </div>
-            <hr />
-            <div className="card-content">
-              <div className={CartCSS["price-calculation-container"]}>
-                <ul className={CartCSS["text"]}>
-                  <li className={CartCSS["price-card-item"]}>
-                    Price (2 Items)
-                  </li>
-                  <li className={CartCSS["price-card-item"]}>Discount</li>
-                  <li className={CartCSS["price-card-item"]}>
-                    Delivery charges
-                  </li>
-                </ul>
-                <ul className={CartCSS["charges"]}>
-                  <li className={CartCSS["price-card-item"]}>Rs.4000/-</li>
-                  <li className={CartCSS["price-card-item"]}>-Rs.1500/-</li>
-                  <li className={CartCSS["price-card-item"]}> Rs.100/-</li>
-                </ul>
-              </div>
-              <hr />
-              <div className={CartCSS["final-price"]}>
-                <h2 className="header-semi-large">TOTAL AMOUNT:</h2>
-                <h2 className="header-semi-large">Rs.2600/-</h2>
-              </div>
-              <hr />
-              <div className={CartCSS["money-save-text"]}>
-                <h2>
-                  You will save{" "}
-                  <span className="header-text header-semi-large">
-                    Rs.1500/-
-                  </span>{" "}
-                  on this order
-                </h2>
-              </div>
-            </div>
-
-            <button className={`btn btn-primary ${CartCSS["place-order-btn"]}`}>
-              PLACE ORDER
-            </button>
-          </div> */}
         </div>
       </div>
     </div>
